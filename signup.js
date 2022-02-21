5ca00452b89a7c4e3cf727ae0e0583ce49e0e5a5
Glitch (glitch-hello-website) <none>
2022-02-21 10:50:06 +0000

    🎏📖 Checkpoint ./signUp.html:35615859/425 ./signup.js:35615859/2105

---
+++ signup.js
@@ -0,0 +1,56 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAs3IB8ZuH0IK-mjU9iIpiWIaFyfdscXAg",
+  authDomain: "computerscience-6e601.firebaseapp.com",
+  databaseURL: "https://computerscience-6e601-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "computerscience-6e601",
+  storageBucket: "computerscience-6e601.appspot.com",
+  messagingSenderId: "208934090461",
+  appId: "1:208934090461:web:505fd1d783390e73ca4d34",
+  measurementId: "G-KXVTYE32XS"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const myDBCxn = firebase.database().ref("/contacts");
+
+// Tell JavaScript to call saveContacts when SUBMIT button is clicked
+const btn = document.getElementById("submit-data");
+btn.addEventListener("click", saveContacts);
+
+// Submit clicked so post the data to the server
+function saveContacts() {
+  alert("SUBMIT clicked!!!");
+
+  
+  const email = document.getElementById("yearIn");
+  const emailValue = email.value;
+  email.value = ""; 
+  email.focus(); 
+  const data = myDBCxn.push();
+  data.set({ email: emailValue });
+  
+   const email = document.getElementById("sportIn");
+  const emailValue = email.value;
+  email.value = ""; 
+  email.focus(); 
+  const data = myDBCxn.push();
+  data.set({ email: emailValue });
+  
+   const email = document.getElementById("nameIn");
+  const emailValue = email.value;
+  name.value = ""; 
+  name.focus(); 
+  const data = myDBCxn.push();
+  data.set({ name: nameValue });
+  
+   const age = document.getElementById("ageIn");
+  const ageValue = age.value;
+  email.value = ""; 
+  email.focus(); 
+  const data = myDBCxn.push();
+  data.set({ age: ageValue });
+  
+  
+}
+
+
