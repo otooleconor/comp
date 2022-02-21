b3a05bbfc96011852a90a929a0a512d1e7be8d07
Glitch (glitch-hello-website) <none>
2022-02-21 10:38:32 +0000

    🍶💂 Checkpoint ./contact.html:35615859/43 ./contact.js:35615859/2538

--- contact.js
+++ contact.js
@@ -1,15 +1,15 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCTT67HbiSoBGYnRfvDmKxGKD6LhdEYwKo",
-  authDomain: "fir-f21be.firebaseapp.com",
-  databaseURL:
-    "https://fir-f21be-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "fir-f21be",
-  storageBucket: "fir-f21be.appspot.com",
-  messagingSenderId: "451019327964",
-  appId: "1:451019327964:web:a5e26b66429864a4bf2719"
+  apiKey: "AIzaSyAs3IB8ZuH0IK-mjU9iIpiWIaFyfdscXAg",
+  authDomain: "computerscience-6e601.firebaseapp.com",
+  databaseURL: "https://computerscience-6e601-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "computerscience-6e601",
+  storageBucket: "computerscience-6e601.appspot.com",
+  messagingSenderId: "208934090461",
+  appId: "1:208934090461:web:505fd1d783390e73ca4d34",
+  measurementId: "G-KXVTYE32XS"
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 
 const myDBCxn = firebase.database().ref("/contacts");
 
@@ -35,62 +35,5 @@ function saveContacts() {
   const data = myDBCxn.push();
   data.set({ email: emailValue });
 }
-<script>
-var myInput = document.getElementById("psw");
-var letter = document.getElementById("letter");
-var capital = document.getElementById("capital");
-var number = document.getElementById("number");
-var length = document.getElementById("length");
 
-// When the user clicks on the password field, show the message box
-myInput.onfocus = function() {
-  document.getElementById("message").style.display = "block";
-}
-
-// When the user clicks outside of the password field, hide the message box
-myInput.onblur = function() {
-  document.getElementById("message").style.display = "none";
-}
-
-// When the user starts to type something inside the password field
-myInput.onkeyup = function() {
-  // Validate lowercase letters
-  var lowerCaseLetters = /[a-z]/g;
-  if(myInput.value.match(lowerCaseLetters)) {
-    letter.classList.remove("invalid");
-    letter.classList.add("valid");
-  } else {
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
-}
-
-  // Validate capital letters
-  var upperCaseLetters = /[A-Z]/g;
-  if(myInput.value.match(upperCaseLetters)) {
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
-  } else {
-    capital.classList.remove("valid");
-    capital.classList.add("invalid");
-  }
-
-  // Validate numbers
-  var numbers = /[0-9]/g;
-  if(myInput.value.match(numbers)) {
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-  } else {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
-  }
-
-  // Validate length
-  if(myInput.value.length >= 8) {
-    length.classList.remove("invalid");
-    length.classList.add("valid");
-  } else {
-    length.classList.remove("valid");
-    length.classList.add("invalid");
-  }
-}
 
